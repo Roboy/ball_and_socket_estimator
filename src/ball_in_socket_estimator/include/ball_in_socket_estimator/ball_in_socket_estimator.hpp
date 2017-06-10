de10a419cad2b55bd79f683cd756d5af0e64258f
letrend <simon.trendel@tum.de>
2017-06-10 20:04:08 +0200

    works with neural net trained on real data

--- src/ball_in_socket_estimator/include/ball_in_socket_estimator/ball_in_socket_estimator.hpp
+++ src/ball_in_socket_estimator/include/ball_in_socket_estimator/ball_in_socket_estimator.hpp
@@ -32,11 +32,22 @@
 #include <fstream>
 #include <iostream>
 #include <mutex>
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include <thread>
+#include <tf/tf.h>
+#include <tf/transform_broadcaster.h>
 
 #endif
 
 using namespace std;
 using namespace cv;
+using namespace Eigen;
+
+struct COLOR{
+    COLOR(float r, float g, float b, float a):r(r),g(g),b(b),a(a){};
+    float r,g,b,a;
+};
 
 class BallInSocketPlugin : public rviz::Panel {
 Q_OBJECT
@@ -64,17 +75,34 @@ Q_SIGNALS:
 public Q_SLOTS:
     void recordData(int state);
     void writeData();
+    /**
+     * Publishes a ray visualization marker
+     * @param pos at this positon
+     * @param dir direction
+     * @param frame in this frame
+     * @param message_id a unique id
+     * @param ns namespace
+     * @param rgda rgb color (0-1) plus transparancy
+     * @param duration for this duration in seconds (0=forever)
+     */
+    void publishRay(Vector3d &pos, Vector3d &dir, const char* frame, const char* ns, int message_id, COLOR color, int duration=0);
 public:
     void poseCallback(const geometry_msgs::PoseConstPtr& msg);
     void magneticSensorCallback(const roboy_communication_middleware::MagneticSensorConstPtr& msg);
 private:
+    void transformPublisher();
     ros::NodeHandle *nh;
-    bool initialized = false, recording = false;
+    bool initialized = false, recording = false, publish_transform = true;
     pair<uint, uint> currentID;
     ros::AsyncSpinner *spinner;
     ros::Subscriber pose_sub, magnetic_sensor_sub;
+    ros::Publisher visualization_pub;
     ofstream data_log;
     geometry_msgs::Pose pose;
     roboy_communication_middleware::MagneticSensor magneticSensors;
     mutex mux;
+    COLOR colors[3] = {COLOR(255,0,0,1), COLOR(0,255,0,1), COLOR(0,0,255,1)};
+    boost::shared_ptr<std::thread> transform_thread;
+    tf::TransformBroadcaster tf_broadcaster;
+    tf::Transform tf_camera;
 };
--- src/ball_in_socket_estimator/src/ball_in_socket_estimator.cpp
+++ src/ball_in_socket_estimator/src/ball_in_socket_estimator.cpp
@@ -15,6 +15,7 @@ BallInSocketPlugin::BallInSocketPlugin(QWidget *parent)
 
     pose_sub = nh->subscribe("/mocap/MarkerPose", 100, &BallInSocketPlugin::poseCallback, this);
     magnetic_sensor_sub = nh->subscribe("/roboy/middleware/MagneticSensor", 100, &BallInSocketPlugin::magneticSensorCallback, this);
+    visualization_pub = nh->advertise<visualization_msgs::Marker>("/visualization_marker", 1);
 
     QObject::connect(this, SIGNAL(newData()), this, SLOT(writeData()));
 
@@ -35,6 +36,17 @@ BallInSocketPlugin::BallInSocketPlugin(QWidget *parent)
     mainLayout->setContentsMargins(0, 0, 0, 0);
 
     this->setLayout(mainLayout);
+
+    tf_camera.setOrigin(tf::Vector3(0, 0, 1.0));
+    tf::Quaternion quat;
+    quat.setRPY(M_PI / 2, 0, M_PI / 2);
+    tf_camera.setRotation(quat);
+
+    publish_transform = true;
+    if(transform_thread==nullptr){
+        transform_thread = boost::shared_ptr<std::thread>(new std::thread(&BallInSocketPlugin::transformPublisher, this));
+        transform_thread->detach();
+    }
 }
 
 BallInSocketPlugin::~BallInSocketPlugin() {
@@ -65,8 +77,8 @@ void BallInSocketPlugin::recordData(int state){
 void BallInSocketPlugin::writeData(){
     std::lock_guard<std::mutex> lock(mux);
     if(data_log.is_open()){
-        data_log << ros::Time::now().nsec << " " << pose.orientation.x << " " << pose.orientation.y << " " << pose.orientation.z << " "
-                << pose.orientation.w << " " << pose.position.x << " " << pose.position.y << " " << pose.position.z << " ";
+        data_log << ros::Time::now().nsec << " " << pose.orientation.x << " " << pose.orientation.y << " " << pose.orientation.z << " " << pose.orientation.w << " "
+                 << pose.position.x << " " << pose.position.y << " " << pose.position.z << " ";
         for(uint i=0; i<magneticSensors.x.size();i++){
             data_log << magneticSensors.x[i] << " " << magneticSensors.y[i] << " " << magneticSensors.z[i] << " ";
         }
@@ -80,6 +92,27 @@ void BallInSocketPlugin::poseCallback(const geometry_msgs::PoseConstPtr &msg) {
         emit newData();
     }
     pose = *msg;
+    visualization_msgs::Marker msg2;
+    msg2.header.frame_id = "world";
+    msg2.ns = "markerModel";
+    msg2.type = visualization_msgs::Marker::MESH_RESOURCE;
+    msg2.color.r = 0.0f;
+    msg2.color.g = 1.0f;
+    msg2.color.b = 0.0f;
+    msg2.color.a = 0.5;
+    msg2.scale.x = 1.0;
+    msg2.scale.y = 1.0;
+    msg2.scale.z = 1.0;
+    msg2.lifetime = ros::Duration();
+    msg2.action = visualization_msgs::Marker::ADD;
+    msg2.header.stamp = ros::Time::now();
+    msg2.id = 81;
+    msg2.pose.position.x = 0;
+    msg2.pose.position.y = 0;
+    msg2.pose.position.z = 0;
+    msg2.pose.orientation = msg->orientation;
+    msg2.mesh_resource = "package://tracking_node/models/markermodel.STL";
+    visualization_pub.publish(msg2);
 }
 
 void BallInSocketPlugin::magneticSensorCallback(const roboy_communication_middleware::MagneticSensorConstPtr& msg){
@@ -88,6 +121,49 @@ void BallInSocketPlugin::magneticSensorCallback(const roboy_communication_middle
 //        emit newData();
 //    }
     magneticSensors = *msg;
+
+    for(uint i=0;i<magneticSensors.x.size();i++){
+        Vector3d pos(0,0,0);
+        Vector3d dir(magneticSensors.x[i]/100.0f, magneticSensors.y[i]/100.0f, magneticSensors.z[i]/100.0f);
+        publishRay(pos,dir,"world","magneticSensors",i,colors[i],1);
+    }
+}
+
+void BallInSocketPlugin::publishRay(Vector3d &pos, Vector3d &dir, const char *frame, const char *ns, int message_id, COLOR color, int duration) {
+    visualization_msgs::Marker arrow;
+    arrow.ns = ns;
+    arrow.type = visualization_msgs::Marker::ARROW;
+    arrow.color.r = color.r;
+    arrow.color.g = color.g;
+    arrow.color.b = color.b;
+    arrow.color.a = color.a;
+    arrow.lifetime = ros::Duration(duration);
+    arrow.scale.x = 0.003;
+    arrow.scale.y = 0.03;
+    arrow.scale.z = 0.03;
+    arrow.action = visualization_msgs::Marker::ADD;
+    arrow.header.stamp = ros::Time::now();
+    arrow.header.frame_id = frame;
+    arrow.id = message_id;
+    arrow.points.clear();
+    geometry_msgs::Point p;
+    p.x = pos(0);
+    p.y = pos(1);
+    p.z = pos(2);
+    arrow.points.push_back(p);
+    p.x += dir(0);
+    p.y += dir(1);
+    p.z += dir(2);
+    arrow.points.push_back(p);
+    visualization_pub.publish(arrow);
+}
+
+void BallInSocketPlugin::transformPublisher(){
+    ros::Rate rate(5);
+    while(publish_transform){
+        tf_broadcaster.sendTransform(tf::StampedTransform(tf_camera, ros::Time::now(), "map", "world"));
+        rate.sleep();
+    }
 }
 
 PLUGINLIB_EXPORT_CLASS(BallInSocketPlugin, rviz::Panel)
