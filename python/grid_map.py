00c8fb0381299a6afb2fe228997b77b56741732e
letrend <st@gi.ai>
2019-03-30 00:52:44 +0100

    grid better

--- python/grid_map.py
+++ python/grid_map.py
@@ -7,7 +7,7 @@ rospy.init_node('msj_platform_grid_map')
 
 sphere_axis0_max = 0.5
 sphere_axis1_max = 0.5
-sphere_axis2_max = 0.6
+sphere_axis2_max = 2.0
 
 sphere0 = rospy.Publisher('/sphere_axis0/sphere_axis0/target', std_msgs.msg.Float32 , queue_size=1)
 sphere1 = rospy.Publisher('/sphere_axis1/sphere_axis1/target', std_msgs.msg.Float32 , queue_size=1)
@@ -30,19 +30,19 @@ while not rospy.is_shutdown():
     msg.data = yaw
     sphere2.publish(msg)
     if roll_dir:
-        roll = roll + 0.001
+        roll = roll + 0.0011
     else:
-        roll = roll - 0.001
+        roll = roll - 0.0011
 
     if pitch_dir:
-        pitch = pitch + 0.001
+        pitch = pitch + 0.000309
     else:
-        pitch = pitch - 0.001
+        pitch = pitch - 0.000309
 
     if yaw_dir:
-        yaw = yaw + 0.001
+        yaw = yaw + 0.000712
     else:
-        yaw = yaw - 0.001
+        yaw = yaw - 0.000712
 
     if abs(roll)>sphere_axis0_max:
         roll_dir = not roll_dir
@@ -51,4 +51,4 @@ while not rospy.is_shutdown():
     if abs(yaw)>sphere_axis0_max:
         yaw_dir = not yaw_dir
 
-    time.sleep(0.01)
+    time.sleep(0.005)
--- python/shoulder_training.py
+++ python/shoulder_training.py
@@ -97,7 +97,7 @@ if record is True:
             (trans,rot2) = listener.lookupTransform('/world', '/top', rospy.Time(0))
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             return
-    
+
         record.write(str(rot[0]) + " " + str(rot[1])+ " " + str(rot[2])+ " " + str(rot[3]) + " " + str(data.x[0])+ " " + str(data.y[0]) + " " + str(data.z[0])+ " " + str(data.x[1])+ " " + str(data.y[1])+ " " + str(data.z[1])+ " " + str(data.x[2])+ " " + str(data.y[2])+ " " + str(data.z[2]) + " " + str(roll) + " " + str(pitch) + " " + str(yaw) + " " +  str(rot2[0]) + " " + str(rot2[1])+ " " + str(rot2[2])+ " " + str(rot2[3]) + "\n")
         rospy.loginfo_throttle(5,str(sample) + " " + str(rot[0]) + " " + str(rot[1])+ " " + str(rot[2])+ " " + str(rot[3]) + " " + str(data.x[0])+ " " + str(data.y[0]) + " " + str(data.z[0])+ " " + str(data.x[1])+ " " + str(data.y[1])+ " " + str(data.z[1])+ " " + str(data.x[2])+ " " + str(data.y[2])+ " " + str(data.z[2]) + " " + str(roll) + " " + str(pitch) + " " + str(yaw) + " " +  str(rot2[0]) + " " + str(rot2[1])+ " " + str(rot2[2])+ " " + str(rot2[3]))
         sensor = np.array([data.x[0], data.y[0], data.z[0], data.x[1], data.y[1], data.z[1], data.x[2], data.y[2], data.z[2]])
@@ -135,14 +135,18 @@ class ball_in_socket_estimator:
     prediction_pub = rospy.Publisher('/visualization_marker', Marker, queue_size=1)
     br = tf.TransformBroadcaster()
     joint_state = rospy.Publisher('/joint_states', sensor_msgs.msg.JointState , queue_size=1)
+    trackingSubscriber = None
+    roll = 0
+    pitch = 0
+    yaw = 0
     def __init__(self):
         global train
         if train:
             self.model = Sequential()
-            self.model.add(Dense(units=30, input_dim=9,kernel_initializer='normal', activation='relu'))
+            self.model.add(Dense(units=100, input_dim=9,kernel_initializer='normal', activation='relu'))
             # model.add(Dropout(dropout))
-            self.model.add(Dense(units=30, kernel_initializer='normal', activation='tanh'))
-            self.model.add(Dense(units=30, kernel_initializer='normal', activation='tanh'))
+            self.model.add(Dense(units=100, kernel_initializer='normal', activation='relu'))
+            self.model.add(Dense(units=100, kernel_initializer='normal', activation='relu'))
             # model.add(Dropout(dropout))
             self.model.add(Dense(units=3,kernel_initializer='normal'))
 
@@ -155,12 +159,13 @@ class ball_in_socket_estimator:
             rospy.loginfo("loading data")
             dataset = pandas.read_csv("/home/letrend/workspace/roboy_control/data0.log", delim_whitespace=True, header=1)
 
-            dataset = dataset.values[:-1,0:]
+            dataset = dataset.values[:len(dataset)-1,0:]
             euler_set = np.array(dataset[:,13:16])
             sensors_set = np.array(dataset[:,4:13])
             print(sensors_set[0,:])
             print(euler_set[0,:])
             # sensors_set = wr.mean_zero(pandas.DataFrame(sensors_set)).values
+
             data_in_train = sensors_set[:int(len(sensors_set)*0.7),:]
             data_in_test = sensors_set[int(len(sensors_set)*0.7):,:]
             data_out_train = euler_set[:int(len(sensors_set)*0.7),:]
@@ -181,8 +186,8 @@ class ball_in_socket_estimator:
             pyplot.legend()
             pyplot.show()
 
-            result = self.model.predict(data_in_test[0,:])
-            print(result)
+            # result = self.model.predict(data_in_test)
+            # print(result)
 
             # serialize model to JSON
             model_json = self.model.to_json()
@@ -192,6 +197,8 @@ class ball_in_socket_estimator:
             self.model.save("model.h5")
             print("Saved model to disk")
         else:
+            self.trackingSubscriber = rospy.Subscriber("joint_states_training", sensor_msgs.msg.JointState, self.trackingCallback)
+
             # load json and create model
             json_file = open('/home/letrend/workspace/roboy_control/src/ball_in_socket_estimator/python/model.json', 'r')
 
@@ -202,16 +209,17 @@ class ball_in_socket_estimator:
             self.model.load_weights("/home/letrend/workspace/roboy_control/src/ball_in_socket_estimator/python/model.h5")
 
             print("Loaded model from disk")
+            self.listener()
 
         self.listener()
     def ros_callback(self, data):
         x_test = np.array([data.x[0], data.y[0], data.z[0], data.x[1], data.y[1], data.z[1], data.x[2], data.y[2], data.z[2]])
         x_test=x_test.reshape((1,9))
         show_ground_truth = True
-        try:
-            (trans,rot2) = listener.lookupTransform('/world', '/top_estimate', rospy.Time(0))
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-            return
+        # try:
+        #     (trans,rot2) = listener.lookupTransform('/world', '/top_estimate', rospy.Time(0))
+        # except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+        #     return
         with self.graph.as_default(): # we need this otherwise the precition does not work ros callback
             euler = self.model.predict(x_test)
 #            pos = self.model.predict(x_test)
@@ -223,7 +231,11 @@ class ball_in_socket_estimator:
             msg.position = [euler[0,0], euler[0,1], euler[0,2]]
             msg.velocity = [0,0,0]
             msg.effort = [0,0,0]
-            # joint_state.publish(msg)
+            error_roll = ((self.roll-euler[0,0])**2)**0.5
+            error_pitch = ((self.pitch-euler[0,1])**2)**0.5
+            error_yaw = ((self.yaw-euler[0,2])**2)**0.5
+            rospy.loginfo_throttle(1, str(error_roll) + " " + str(error_pitch) + " " + str(error_yaw) )
+            self.joint_state.publish(msg)
 #             norm = numpy.linalg.norm(quat)
 #             q = (quat[0,0]/norm,quat[0,1]/norm,quat[0,2]/norm,quat[0,3]/norm)
 # #            print "predicted: ",(pos[0,0],pos[0,1],pos[0,2])
@@ -232,6 +244,11 @@ class ball_in_socket_estimator:
 #                      rospy.Time.now(),
 #                      "top_NN",
 #                      "world")
+    def trackingCallback(self,data):
+        self.roll = data.position[0]
+        self.pitch = data.position[1]
+        self.yaw = data.position[2]
+        rospy.loginfo_throttle(5, "receiving tracking data")
 
     def listener(self):
         rospy.Subscriber("roboy/middleware/MagneticSensor", MagneticSensor, self.ros_callback)
