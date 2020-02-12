09131a5180e561e7143d2a565684d6003f3a3aea
letrend <st@gi.ai>
2020-02-12 19:35:33 +0100

    with runningmean

--- python/predict.py
+++ python/predict.py
@@ -29,6 +29,7 @@ class ball_in_socket_estimator:
     graph = tensorflow.get_default_graph()
     # base_path= '/home/letrend/workspace/roboy3/src/ball_in_socket_estimator/python/'
     joint_state = rospy.Publisher('/external_joint_states', sensor_msgs.msg.JointState , queue_size=1)
+    msg = sensor_msgs.msg.JointState()
     def __init__(self):
         # load json and create model
         json_file = open(self.base_path+self.network_name+'.json', 'r')
@@ -38,7 +39,9 @@ class ball_in_socket_estimator:
         # load weights into new model
         self.model.load_weights(self.base_path+self.model_name+".h5")
         print("Loaded model from disk")
+        self.msg.position = [0,0,0]
         self.listener()
+
     def magneticsCallback(self, data):
         x_test = np.array([data.x[0], data.y[0], data.z[0], data.x[1], data.y[1], data.z[1], data.x[2], data.y[2], data.z[2], data.x[3], data.y[3], data.z[3]])
         x_test=x_test.reshape((1,12))
@@ -46,16 +49,16 @@ class ball_in_socket_estimator:
             euler = self.model.predict(x_test)
             #            pos = self.model.predict(x_test)
             rospy.loginfo_throttle(1, (euler[0,0],euler[0,1],euler[0,2]))
-            msg = sensor_msgs.msg.JointState()
-            msg.header = std_msgs.msg.Header()
-            msg.header.stamp = rospy.Time.now()
-            msg.name = [self.model_name+'_axis0', self.model_name+'_axis1', self.model_name+'_axis2']
-            msg.position = [euler[0,0], euler[0,1], euler[0,2]]
-            for i in range(len(msg.position)):
-                msg.position[i] += self.offset[i]
-            msg.velocity = [0,0,0]
-            msg.effort = [0,0,0]
-            self.joint_state.publish(msg)
+
+            self.msg.header = std_msgs.msg.Header()
+            self.msg.header.stamp = rospy.Time.now()
+            self.msg.name = [self.model_name+'_axis0', self.model_name+'_axis1', self.model_name+'_axis2']
+            self.msg.position = [0.9*self.msg.position[0]+0.1*euler[0,0], 0.9*self.msg.position[1]+0.1*euler[0,1], 0.9*self.msg.position[2]+0.1*euler[0,2]]
+            for i in range(len(self.msg.position)):
+                self.msg.position[i] += self.offset[i]
+            self.msg.velocity = [0,0,0]
+            self.msg.effort = [0,0,0]
+            self.joint_state.publish(self.msg)
 
     def listener(self):
         rospy.Subscriber("roboy/middleware/MagneticSensor", MagneticSensor, self.magneticsCallback)
