a0e12931eb2d77c8610a774da657c93dff6893ac
letrend <st@gi.ai>
2020-05-22 21:07:10 +0200

    6dof calibration

---
+++ python/magnetic_field_calibration.py
@@ -0,0 +1,133 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from magpylib.source.magnet import Box,Cylinder
+from magpylib import Collection, displaySystem, Sensor
+from scipy.optimize import fsolve, least_squares
+import matplotlib.animation as manimation
+import random
+import math, os, time
+import geometry_msgs
+from multiprocessing import Pool, freeze_support
+from pathlib import Path
+
+b_target = [[6.235,5.522,16.281],[-1.474,2.147,37.418],[-5.472,-2.943,-36.025],[7.067,-6.949,-17.665]]
+
+def gen_sensors(offset,angles):
+    sensor_pos = [[-22.7,7.7,0],[-14.7,-19.4,0],[14.7,-19.4,0],[22.7,7.7,0]]
+    sensors = []
+    i = 0
+    for pos in sensor_pos:
+        s = Sensor(pos=(pos[0]+offset[i][0],pos[1]+offset[i][1],pos[2]+offset[i][2]),angle=90,axis=(0,0,1))
+        s.rotate(angle=angles[0][i],axis=(1,0,0))
+        s.rotate(angle=angles[1][i],axis=(0,1,0))
+        s.rotate(angle=angles[2][i],axis=(0,0,1))
+        sensors.append(s)
+        i = i+1
+    return sensors
+
+def gen_magnets(fieldstrength,offset,angles):
+    magnets = []
+    field = [(0,-fieldstrength[0],0),(0,0,fieldstrength[1]),(0,0,-fieldstrength[2])]
+    for i in range(0,3):
+        magnet = Box(mag=field[i],dim=(10,10,10),\
+        pos=(13*math.sin(i*(360/3)/180.0*math.pi)+offset[i][0],13*math.cos(i*(360/3)/180.0*math.pi+offset[i][1]),offset[i][2]),\
+        angle=i*(360/3))
+        magnet.rotate(angle=angles[0][i],axis=(1,0,0))
+        magnet.rotate(angle=angles[1][i],axis=(0,1,0))
+        magnet.rotate(angle=angles[2][i],axis=(0,0,1))
+        magnets.append(magnet)
+    return magnets
+
+def func(x):
+    fieldstrength = x[0:3]
+    magnet_offsets = [[x[3],x[4],x[5]],[x[6],x[7],x[8]],[x[9],x[10],x[11]]]
+    magnet_angles = [[x[12],x[13],x[14]],[x[15],x[16],x[17]],[x[18],x[19],x[20]]]
+    sensor_offsets = [[x[21],x[22],x[23]],[x[24],x[25],x[26]],[x[27],x[28],x[29]],[x[30],x[31],x[32]]]
+    sensor_angles = [[x[33],x[34],x[35],x[36]],[x[37],x[38],x[39],x[40]],[x[41],x[42],x[43],x[44]]]
+    sensors = gen_sensors(sensor_offsets,sensor_angles)
+    c = Collection(gen_magnets(x,magnet_offsets,magnet_angles))
+    b_error = 0
+    i = 0
+    for sens in sensors:
+        b_error = b_error + np.linalg.norm(sens.getB(c)-b_target[i])
+        i=i+1
+    # print(b_error)
+    return [b_error,b_error,b_error]
+
+res = least_squares(func, \
+[1300,1300,1300,\
+0,0,0,0,0,0,0,0,0,\
+0,0,0,0,0,0,0,0,0,\
+0,0,0,0,0,0,0,0,0,0,0,0,\
+0,0,0,0,0,0,0,0,0,0,0,0\
+],\
+bounds = \
+((1000,1000,1000,\
+-1,-1,-1,-1,-1,-1,-1,-1,-1,\
+-5,-5,-5,-5,-5,-5,-5,-5,-5,\
+-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,\
+-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1\
+), \
+(2000,2000,2000,\
+1,1,1,1,1,1,1,1,1,\
+5,5,5,5,5,5,5,5,5,\
+1,1,1,1,1,1,1,1,1,1,1,1,\
+1,1,1,1,1,1,1,1,1,1,1,1\
+)), ftol=1e-10, xtol=1e-10,verbose=2)
+
+magnet_offsets = [[res.x[3],res.x[4],res.x[5]],[res.x[6],res.x[7],res.x[8]],[res.x[9],res.x[10],res.x[11]]]
+magnet_angles = [[res.x[12],res.x[13],res.x[14]],[res.x[15],res.x[16],res.x[17]],[res.x[18],res.x[19],res.x[20]]]
+sensor_offsets = [[res.x[21],res.x[22],res.x[23]],[res.x[24],res.x[25],res.x[26]],[res.x[27],res.x[28],res.x[29]],[res.x[30],res.x[31],res.x[32]]]
+sensor_angles = [[res.x[33],res.x[34],res.x[35],res.x[36]],[res.x[37],res.x[38],res.x[39],res.x[40]],[res.x[41],res.x[42],res.x[43],res.x[44]]]
+print("b_field without calibration:")
+c = Collection(gen_magnets([1300,1300,1300],[[0,0,0],[0,0,0],[0,0,0]],[[0,0,0],[0,0,0],[0,0,0]]))
+sensors = gen_sensors([[0,0,0],[0,0,0],[0,0,0],[0,0,0]],[[0,0,0,0],[0,0,0,0],[0,0,0,0]])
+for sens in sensors:
+    print(sens.getB(c))
+print("b_field_error without calibration: %f\n"%func([1300,1300,1300,\
+0,0,0,0,0,0,0,0,0,\
+0,0,0,0,0,0,0,0,0,\
+0,0,0,0,0,0,0,0,0,0,0,0,\
+0,0,0,0,0,0,0,0,0,0,0,0,\
+])[0])
+print("b_field with calibration:")
+c = Collection(gen_magnets(res.x[0:3],magnet_offsets,magnet_angles))
+sensors = gen_sensors(sensor_offsets,sensor_angles)
+i = 0
+for sens in sensors:
+    print(sens.getB(c))
+    i = i+1
+print("b_field_error with calibration: %f\n"%func(res.x)[0])
+print("b_field target:")
+i = 0
+for sens in sensors:
+    print(b_target[i])
+    i = i+1
+print("\noptimization results: \n\
+fieldstrength %.3f %.3f %.3f\n\
+magnet offsets:\n \
+%.3f %.3f %.3f\n \
+%.3f %.3f %.3f\n \
+%.3f %.3f %.3f\n \
+magnet angles:\n \
+%.3f %.3f %.3f\n \
+%.3f %.3f %.3f\n \
+%.3f %.3f %.3f\n \
+sensor offsets:\n \
+%.3f %.3f %.3f\n \
+%.3f %.3f %.3f\n \
+%.3f %.3f %.3f\n \
+%.3f %.3f %.3f\n \
+sensor angles:\n \
+%.3f %.3f %.3f\n \
+%.3f %.3f %.3f\n \
+%.3f %.3f %.3f\n \
+%.3f %.3f %.3f\n \
+"%\
+(res.x[0],res.x[1],res.x[2],\
+res.x[3],res.x[4],res.x[5],res.x[6],res.x[7],res.x[8],res.x[9],res.x[10],res.x[11],\
+res.x[12],res.x[13],res.x[14],res.x[15],res.x[16],res.x[17],res.x[18],res.x[19],res.x[20],\
+res.x[21],res.x[22],res.x[23],res.x[24],res.x[25],res.x[26],res.x[27],res.x[28],res.x[29],res.x[30],res.x[31],res.x[32],\
+res.x[33],res.x[34],res.x[35],res.x[36],res.x[37],res.x[38],res.x[39],res.x[40],res.x[41],res.x[42],res.x[43],res.x[44],\
+))
