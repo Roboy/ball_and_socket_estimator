a34376e87ba84fcfd2c65dca49530fd183047e4d
letrend <st@gi.ai>
2020-05-24 13:33:28 +0200

    finds simulated calibration values

--- python/magnetic_field_calibration_reduced.py
+++ python/magnetic_field_calibration_reduced.py
@@ -49,16 +49,16 @@ def gen_magnets(fieldstrength,offset,angles):
     return magnets
 
 def func(x):
-    fieldstrength = x[0:3]
-    magnet_offsets = [[x[3],x[4],x[5]],[x[6],x[7],x[8]],[x[9],x[10],x[11]]]
-    magnet_angles = [[x[12],x[13],x[14]],[x[15],x[16],x[17]],[x[18],x[19],x[20]]]
-    sensor_offsets = [[x[21],x[22],x[23]],[x[24],x[25],x[26]],[x[27],x[28],x[29]],[x[30],x[31],x[32]]]
-    sensor_angles = [[x[33],x[34],x[35]],[x[36],x[37],x[38]],[x[39],x[40],x[41]],[x[42],x[43],x[44]]]
+    fieldstrength = [1300,1300,1300]
+    magnet_offsets = [[0,0,x[0]],[0,0,x[1]],[0,0,x[2]]]
+    magnet_angles = [[x[3],x[4],0],[x[5],x[6],0],[x[7],x[8],0]]
+    sensor_offsets = [[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
+    sensor_angles = [[0,0,x[9]],[0,0,x[10]],[0,0,x[11]],[0,0,x[12]]]
     b_error = 0
     j =0
     for pos in sensor_log['position']:
         sensors = gen_sensors(sensor_offsets,sensor_angles)
-        c = Collection(gen_magnets(x,magnet_offsets,magnet_angles))
+        c = Collection(gen_magnets(fieldstrength,magnet_offsets,magnet_angles))
         c.rotate(angle=pos[0],axis=(1,0,0), anchor=(0,0,0))
         c.rotate(angle=pos[1],axis=(0,1,0), anchor=(0,0,0))
         c.rotate(angle=pos[2],axis=(0,0,1), anchor=(0,0,0))
@@ -72,49 +72,45 @@ def func(x):
     return [b_error,b_error,b_error]
 
 res = least_squares(func, \
-[1300,1300,1300,\
-0,0,0,0,0,0,0,0,0,\
-0,0,0,0,0,0,0,0,0,\
-0,0,0,0,0,0,0,0,0,0,0,0,\
-0,0,0,0,0,0,0,0,0,0,0,0\
+[0,0,0,\
+0,0,0,0,0,0,\
+0,0,0,0\
 ],\
 bounds = \
-((1000,1000,1000,\
--1,-1,-1,-1,-1,-1,-1,-1,-1,\
--5,-5,-5,-5,-5,-5,-5,-5,-5,\
--1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,\
--1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1\
+((-5,-5,-5,\
+-5,-5,-5,-5,-5,-5,\
+-5,-5,-5,-5\
 ), \
-(2000,2000,2000,\
-1,1,1,1,1,1,1,1,1,\
-5,5,5,5,5,5,5,5,5,\
-1,1,1,1,1,1,1,1,1,1,1,1,\
-1,1,1,1,1,1,1,1,1,1,1,1\
-)), ftol=1e-9, xtol=1e-9,verbose=2)
+(5,5,5,\
+5,5,5,5,5,5,\
+5,5,5,5\
+)), ftol=1e-8, xtol=1e-8,verbose=2,max_nfev=2000)
 
-magnet_offsets = [[res.x[3],res.x[4],res.x[5]],[res.x[6],res.x[7],res.x[8]],[res.x[9],res.x[10],res.x[11]]]
-magnet_angles = [[res.x[12],res.x[13],res.x[14]],[res.x[15],res.x[16],res.x[17]],[res.x[18],res.x[19],res.x[20]]]
-sensor_offsets = [[res.x[21],res.x[22],res.x[23]],[res.x[24],res.x[25],res.x[26]],[res.x[27],res.x[28],res.x[29]],[res.x[30],res.x[31],res.x[32]]]
-sensor_angles = [[res.x[33],res.x[34],res.x[35],res.x[36]],[res.x[37],res.x[38],res.x[39],res.x[40]],[res.x[41],res.x[42],res.x[43],res.x[44]]]
+magnet_offsets = [[0,0,res.x[0]],[0,0,res.x[1]],[0,0,res.x[2]]]
+magnet_angles = [[res.x[3],res.x[4],0],[res.x[5],res.x[6],0],[res.x[7],res.x[8],0]]
+sensor_offsets = [[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
+sensor_angles = [[0,0,res.x[9]],[0,0,res.x[10]],[0,0,res.x[11]],[0,0,res.x[12]]]
 print("b_field without calibration:")
 c = Collection(gen_magnets([1300,1300,1300],[[0,0,0],[0,0,0],[0,0,0]],[[0,0,0],[0,0,0],[0,0,0]]))
 sensors = gen_sensors([[0,0,0],[0,0,0],[0,0,0],[0,0,0]],[[0,0,0],[0,0,0],[0,0,0],[0,0,0]])
 for sens in sensors:
     print(sens.getB(c))
-print("b_field_error without calibration: %f\n"%func([1300,1300,1300,\
-0,0,0,0,0,0,0,0,0,\
-0,0,0,0,0,0,0,0,0,\
-0,0,0,0,0,0,0,0,0,0,0,0,\
-0,0,0,0,0,0,0,0,0,0,0,0,\
+print("b_field_error without calibration: %f\n"%func([\
+0,0,0,0,0,0,0,0,0,0,0,0,0\
 ])[0])
 print("b_field with calibration:")
-c = Collection(gen_magnets(res.x[0:3],magnet_offsets,magnet_angles))
+c = Collection(gen_magnets([1300,1300,1300],magnet_offsets,magnet_angles))
 sensors = gen_sensors(sensor_offsets,sensor_angles)
 i = 0
 for sens in sensors:
     print(sens.getB(c))
     i = i+1
 print("b_field_error with calibration: %f\n"%func(res.x)[0])
+print("target b_field:")
+i = 0
+for sens in sensors:
+    print(sensor_log['sensor_values'][0][i])
+    i = i+1
 print("\noptimization results: \n\
 fieldstrength %.3f %.3f %.3f\n\
 magnet offsets:\n \
@@ -136,9 +132,12 @@ sensor angles:\n \
 %.3f %.3f %.3f\n \
 %.3f %.3f %.3f\n \
 "%\
-(res.x[0],res.x[1],res.x[2],\
-res.x[3],res.x[4],res.x[5],res.x[6],res.x[7],res.x[8],res.x[9],res.x[10],res.x[11],\
-res.x[12],res.x[13],res.x[14],res.x[15],res.x[16],res.x[17],res.x[18],res.x[19],res.x[20],\
-res.x[21],res.x[22],res.x[23],res.x[24],res.x[25],res.x[26],res.x[27],res.x[28],res.x[29],res.x[30],res.x[31],res.x[32],\
-res.x[33],res.x[34],res.x[35],res.x[36],res.x[37],res.x[38],res.x[39],res.x[40],res.x[41],res.x[42],res.x[43],res.x[44],\
+(1300,1300,1300,\
+0,0,res.x[0],0,0,res.x[1],0,0,res.x[2],\
+res.x[3],res.x[4],0,res.x[5],res.x[6],0,res.x[7],res.x[8],0,\
+0,0,0,0,0,0,0,0,0,0,0,0,\
+0,0,res.x[9],\
+0,0,res.x[10],\
+0,0,res.x[11],\
+0,0,res.x[12]\
 ))
